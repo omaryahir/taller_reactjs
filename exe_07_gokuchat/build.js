cc0ba2af41699e1a4ec14ab64aa7d76fb99913fc
omaryahir <omaryahir@gmail.com>
2016-01-20 15:43:19 -0600

    ADD: css reset

--- exe_07_gokuchat/build.js
+++ exe_07_gokuchat/build.js
@@ -63,7 +63,7 @@ var GokuApp = function (_React$Component) {
         var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(GokuApp).call(this, props));
 
         _this.state = { messages: [] };
-        //this.onGrowl = this.onGrowl.bind(this);
+        _this.onGrowl = _this.onGrowl.bind(_this);
 
         _this.gokus = [{ number: 1, name: 'Super Saiyan 1' }, { number: 2, name: 'Super Saiyan 2' }, { number: 3, name: 'Super Saiyan 3' }, { number: 4, name: 'Super Saiyan 4' }];
         return _this;
